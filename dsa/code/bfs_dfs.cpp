060139e237b74f840b175e1abb825c6b3c60cf44
shivamraj6288 <shivamraj.study@gmail.com>
2020-07-31 00:27:02 +0530

    initial commit

---
+++ dsa/code/bfs_dfs.cpp
@@ -0,0 +1,24 @@
+#include "bits/stdc++.h"
+using namespace  std;
+
+void bfs (vector<int>data , int u){
+
+
+}
+
+int main (int argc, char *argsv[]){
+
+
+    int n ;
+    cin >> n;
+    vector <vector<int>> data(n) ;
+    cout << v.size();
+    int edges;
+    data.push_back[0].push_back1;
+    for (int i=1; i<=edges; i++){
+        int v1, v2;
+        cin >> v1 >> v2;
+        data[]
+    }
+
+}
